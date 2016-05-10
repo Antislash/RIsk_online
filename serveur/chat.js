86b511b80f64e56b2f7825da5b753f974e41936b
Gataf <gtaffor@gmail.com>
2016-05-10 17:04:11 +0200

    Nouveau depart en php

--- serveur/chat.js
+++
@@ -1,7 +0,0 @@
-var main = require('./main.js');
-
-var io = require('socket.io').listen(main.server);
-
-io.sockets.on('connection', function(socket) {
-  console.log('Un utilsateur est connecté au serveur !');
-}); 
