3c76799c9e9417e52b876435360327fa0909e77f
Anuswan91 <vivien.blum@free.fr>
2016-05-10 20:15:26 +0200

    Développement de login en php (inacheve)

--- serveur/connexion.php
+++ serveur/connexion.php
@@ -1,16 +1,8 @@
 <?php
-
-try
-    {
-
-        $bdd = new PDO('mysql:host=localhost;dbname=risk;charset=utf8', 'root', '');
-
-    }
-catch (Exception $e)
-    {
-
-        die('Erreur : ' . $e->getMessage());
-
-    }
-
+try{
+	$bdd = new PDO('mysql:host=localhost;dbname=risk;charset=utf8', 'root', '');
+}
+catch (Exception $e){
+	die('Erreur : ' . $e->getMessage());
+}
 ?>
