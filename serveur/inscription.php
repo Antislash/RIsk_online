763a038ae59951d80de4ae91807aba9fccfa0948
Gataf <gtaffor@gmail.com>
2016-05-11 10:54:38 +0200

    ajout fichier inscription

---
+++ serveur/inscription.php
@@ -0,0 +1,5 @@
+<?php
+include "connexion.php";
+
+header('Location: ')
+?>
--- serveur/login.php
+++ serveur/login.php
@@ -1,24 +1,23 @@
+<?php
+include "connexion.php";
 
-<?php 
-	include "connexion.php";
-	
-	//Exemple TEST
-	$_POST['connexionPseudo'] = 'Luc';
-	$_POST['connexionPassword'] = 'test';
-	if(isset($_POST['connexionPseudo']) && isset($_POST['connexionPassword'])){
-		$pseudo = filter_var($_POST['connexionPseudo'], FILTER_SANITIZE_STRING);
-		$password = filter_var($_POST['connexionPassword'], FILTER_SANITIZE_STRING);
-		$user = $bdd->query("SELECT * FROM user WHERE pseudo = '".$pseudo."'");
-		/*foreach($user as $us){
-			var_dump($us);
-		}*/
-		if($user == false){
-			//l'user n'existe pas
-			//TODO renvoyer vers inscription
-		}
-		else if(true){	//TODO comparer les password
-			
-		}
-		//var_dump($user);
-	}
+//Exemple TEST
+var_dump($_POST['connexionPseudo']);
+//$_POST['connexionPseudo'] = 'Luc';
+//$_POST['connexionPassword'] = 'test';
+if (isset($_POST['connexionPseudo']) && isset($_POST['connexionPassword'])) {
+    $pseudo = filter_var($_POST['connexionPseudo'], FILTER_SANITIZE_STRING);
+    $password = filter_var($_POST['connexionPassword'], FILTER_SANITIZE_STRING);
+    $user = $bdd->query("SELECT * FROM user WHERE pseudo = '" . $pseudo . "'");
+    /*foreach($user as $us){
+        var_dump($us);
+    }*/
+    if ($user == false) {
+        //l'user n'existe pas
+        //TODO renvoyer vers inscription
+    } else if (true) {    //TODO comparer les password
+
+    }
+    //var_dump($user);
+}
 ?>
