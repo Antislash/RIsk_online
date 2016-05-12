61f51e81926d5083bbcdbe08e37627e0a59224b4
Anuswan91 <vivien.blum@free.fr>
2016-05-12 17:14:36 +0200

    modif ajout_ami

--- www/include/ajout_ami.php
+++ www/include/ajout_ami.php
@@ -5,18 +5,23 @@
 		$req = $bdd->query("SELECT * FROM user where usr_pseudo = '".$_GET['pseudoSearch']."'");
 		$user_ami = $req->fetch();
 		if($user_ami != false){
-			$req2 = $bdd->query("SELECT * FROM user1_has_user2 WHERE (id_usr1 = ".$_SESSION['usr_id']." AND id_usr2 = ".$user_ami['usr_id'].")");
-			if($req2->fetch() == false){
-				$insert1 = "INSERT INTO user1_has_user2 (id_usr1, id_usr2) VALUES (".$_SESSION['usr_id'].",". $user_ami['usr_id'].")";
-				$insert2 = "INSERT INTO user1_has_user2 (id_usr1, id_usr2) VALUES (".$user_ami['usr_id'].",". $_SESSION['usr_id'].")";
-				$req1 = $bdd->exec($insert1);
-				$req2 = $bdd->exec($insert2);
-				if($req1 != false && $req2 != false){
-					echo "Vous etes ami avec '".$user_ami['usr_pseudo']."'";
-				}
+			if($user_ami['usr_id'] == $_SESSION['usr_id']){
+				echo "Vous ne pouvez pas etre ami avec vous-meme";
 			}
 			else{
-				echo "Vous etes deja ami avec '".$user_ami['usr_pseudo']."'";
+				$req2 = $bdd->query("SELECT * FROM user1_has_user2 WHERE (id_usr1 = ".$_SESSION['usr_id']." AND id_usr2 = ".$user_ami['usr_id'].")");
+				if($req2->fetch() == false){
+					$insert1 = "INSERT INTO user1_has_user2 (id_usr1, id_usr2) VALUES (".$_SESSION['usr_id'].",". $user_ami['usr_id'].")";
+					$insert2 = "INSERT INTO user1_has_user2 (id_usr1, id_usr2) VALUES (".$user_ami['usr_id'].",". $_SESSION['usr_id'].")";
+					$req1 = $bdd->exec($insert1);
+					$req2 = $bdd->exec($insert2);
+					if($req1 != false && $req2 != false){
+						echo "Vous etes ami avec '".$user_ami['usr_pseudo']."'";
+					}
+				}
+				else{
+					echo "Vous etes deja ami avec '".$user_ami['usr_pseudo']."'";
+				}
 			}
 		}
 		else{
