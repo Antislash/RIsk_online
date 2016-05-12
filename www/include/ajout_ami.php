31b750380fdbc08375740d84d871076cee84d340
Anuswan91 <vivien.blum@free.fr>
2016-05-12 16:46:47 +0200

    Ajout ami fini

--- www/include/ajout_ami.php
+++ www/include/ajout_ami.php
@@ -1,3 +1,29 @@
-<?php 
-	echo "bonjour les enfants";
+<?php
+	session_start();
+	include "../../php/connexion.php";
+	if(isset($_GET['pseudoSearch']) && isset($_SESSION['usr_id'])){
+		$req = $bdd->query("SELECT * FROM user where usr_pseudo = '".$_GET['pseudoSearch']."'");
+		$user_ami = $req->fetch();
+		if($user_ami != false){
+			$req2 = $bdd->query("SELECT * FROM user1_has_user2 WHERE (id_usr1 = ".$_SESSION['usr_id']." AND id_usr2 = ".$user_ami['usr_id'].")");
+			if($req2->fetch() == false){
+				$insert1 = "INSERT INTO user1_has_user2 (id_usr1, id_usr2) VALUES (".$_SESSION['usr_id'].",". $user_ami['usr_id'].")";
+				$insert2 = "INSERT INTO user1_has_user2 (id_usr1, id_usr2) VALUES (".$user_ami['usr_id'].",". $_SESSION['usr_id'].")";
+				$req1 = $bdd->exec($insert1);
+				$req2 = $bdd->exec($insert2);
+				if($req1 != false && $req2 != false){
+					echo "Vous etes ami avec '".$user_ami['usr_pseudo']."'";
+				}
+			}
+			else{
+				echo "Vous etes deja ami avec '".$user_ami['usr_pseudo']."'";
+			}
+		}
+		else{
+			echo 'Le joueur \''.$_GET['pseudoSearch'].'\' n\'existe pas.';
+		}
+	}
+	else{
+		echo "";
+	}
 ?>
--- www/include/contact.php
+++ www/include/contact.php
@@ -1,7 +1,7 @@
 		<div id="friends">
 			<div id="languette">
 				<?php 
-					$nb_amis = $bdd->query('SELECT COUNT(1) as nb FROM user usr INNER JOIN user1_has_user2 frd ON frd.id_usr2 = usr.usr_id WHERE (usr.code_sta = \'gam\' OR usr.code_sta = \'on\') AND frd.id_usr1 = 2 GROUP BY frd.id_usr1');
+					$nb_amis = $bdd->query('SELECT COUNT(1) as nb FROM user usr INNER JOIN user1_has_user2 frd ON frd.id_usr2 = usr.usr_id WHERE (usr.code_sta = \'gam\' OR usr.code_sta = \'on\') AND frd.id_usr1 = '.$_SESSION['usr_id'].' GROUP BY frd.id_usr1');
 					$nb_ami = $nb_amis->fetch();
 				?>
 				<span id="deroule" onclick="derouler();">Contacts (<?php echo $nb_ami['nb'];?>)</span>
@@ -9,7 +9,7 @@
 			<div id="friends_scroll" class="null">
 				<table id="list_friends" cellspacing="0">
 				<?php
-					$amis = $bdd->query('SELECT usr.usr_pseudo, img.img_chemin, sta.sta_nom, sta.sta_class, DATEDIFF(CURDATE(), usr.usr_date_inscription) FROM user usr INNER JOIN user1_has_user2 frd ON frd.id_usr2 = usr.usr_id INNER JOIN image img ON img.img_id = usr.id_img INNER JOIN statut_user sta ON sta.sta_code = usr.code_sta WHERE frd.id_usr1 = 2 ORDER BY sta.sta_code desc');
+					$amis = $bdd->query('SELECT usr.usr_pseudo, img.img_chemin, sta.sta_nom, sta.sta_class, DATEDIFF(CURDATE(), usr.usr_date_inscription) FROM user usr INNER JOIN user1_has_user2 frd ON frd.id_usr2 = usr.usr_id INNER JOIN image img ON img.img_id = usr.id_img INNER JOIN statut_user sta ON sta.sta_code = usr.code_sta WHERE frd.id_usr1 = '.$_SESSION['usr_id'].' ORDER BY sta.sta_code desc');
 					while($ami = $amis->fetch()){
 						echo "<tr class=\"bloc_friends\">";
 						echo "<td class=\"avatar_friends\">";
@@ -24,8 +24,8 @@
 				?>
 				</table>
 				<form id="add_friends">
-					<input class="textbox" id="search_user" onkeyup="searchUser();" type="text" name="friend" maxlength=20/>
-					<input class="button" type="submit" value="+">
+					<input class="textbox" id="search_user" type="text" name="friend" maxlength=20/>
+					<input class="button" onclick="searchUser();" value="+">
 				</form>
 			</div>
 		</div>
@@ -40,9 +40,6 @@
 				elm.className = "unwrap";
 			}
 		}
-		/*$(document).ready(function(){
-				$("#search_user").hide();
-			});*/
 		function getXhr(){
 			var xhr = null; 
 			if(window.XMLHttpRequest) // Firefox et autres
@@ -63,17 +60,20 @@
 			
 		function searchUser(){
 			var xhr = getXhr();
-			var url = "C:/wamp/www/RIsk_online/www/include/ajout_ami.php?";
+			var pathname = window.location.pathname;
+			pathname = pathname.substring(0,17);
+			var url = pathname+"/include/ajout_ami.php?";
+			var urlH = window.location.href;
 			//on construit l'url avec les params en GET
 			if($("#search_user").val() != null && $("#search_user").val() != ""){
 				url = url + "pseudoSearch=" + $("#search_user").val();
 			}
-			//alert(url);
 			xhr.onreadystatechange = function(){
 				// On ne fait quelque chose que si on a tout re�u et que le serveur est ok
 				if(xhr.readyState == 4 && xhr.status == 200){
-					//$(reponse).html(xhr.responseText);	//On �crit la reponse de la page "traitement.php"
-					alert(xhr.responseText);
+					if(xhr.responseText != ""){
+						notif(xhr.responseText,1);
+					}
 				}
 			}
 			xhr.open("GET",url,true);	//On lance la page en GET
