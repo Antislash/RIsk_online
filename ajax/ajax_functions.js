46ed89a3d50394425b572f81ea7f0ff4ba105542
Vivien Blum <vblum@a-109977.ups.u-psud.fr>
2016-05-13 12:48:55 +0200

    fonction requete ajax finie

---
+++ ajax/ajax_functions.js
@@ -0,0 +1,51 @@
+function getXhr(){
+	var xhr = null; 
+	if(window.XMLHttpRequest) // Firefox et autres
+	   xhr = new XMLHttpRequest(); 
+	else if(window.ActiveXObject){ // Internet Explorer 
+	   try {
+				xhr = new ActiveXObject("Msxml2.XMLHTTP");
+			} catch (e) {
+				xhr = new ActiveXObject("Microsoft.XMLHTTP");
+			}
+	}
+	else { // XMLHttpRequest non support� par le navigateur 
+	   alert("Votre navigateur ne supporte pas les objets XMLHTTPRequest..."); 
+	   xhr = false; 
+	}
+	return xhr;
+}
+
+function getHostProjectName(projectName = "/RIsk_online"){
+	var pathname = window.location.pathname;
+	pathname = pathname.substr(0,pathname.indexOf(projectName)+projectName.length);
+	return pathname;
+}
+
+function reqAjax(param, pageActionPhp, action){
+	if( (typeof param === "object") && (param !== null) )
+	{
+		var xhr = getXhr();
+		var url = getHostProjectName() + pageActionPhp + "?";
+		for (var id in param) {
+			url = url + id+"="+param[id]+"&";
+			//alert(id+" = "+param[id]);
+		}
+		//On enleve le dernier "&"
+		url = url.substr(0, url.length-1);
+		xhr.onreadystatechange = function(){
+		// On ne fait quelque chose que si on a tout re�u et que le serveur est ok
+		if(xhr.readyState == 4 && xhr.status == 200){
+			if(xhr.responseText != ""){
+				action(xhr.responseText,1);
+			}
+		}
+	}
+	xhr.open("GET",url,true);	//On lance la page en GET
+	xhr.send(null);
+	}
+	else{
+		return false;
+	}
+}
+
--- www/include/contact.php
+++ www/include/contact.php
@@ -3,6 +3,9 @@
 				<?php 
 					$nb_amis = $bdd->query('SELECT COUNT(1) as nb FROM user usr INNER JOIN user1_has_user2 frd ON frd.id_usr2 = usr.usr_id WHERE (usr.code_sta = \'gam\' OR usr.code_sta = \'on\') AND frd.id_usr1 = '.$_SESSION['usr_id'].' GROUP BY frd.id_usr1');
 					$nb_ami = $nb_amis->fetch();
+					if($nb_ami == false){
+						$nb_ami['nb'] = 0;
+					}
 				?>
 				<span id="deroule" onclick="derouler();">Contacts (<?php echo $nb_ami['nb'];?>)</span>
 			</div>
@@ -25,12 +28,14 @@
 				</table>
 				<form id="add_friends">
 					<input class="textbox" id="search_user" type="text" name="friend" maxlength=20/>
-					<input class="button" onclick="searchUser();" value="+">
+					<input class="button" id="submit_friend" onclick="searchUser();" value="+">
 				</form>
 			</div>
 		</div>
 		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
-		<<script type="text/javascript">
+		<script type="text/javascript" src="../ajax/ajax_functions.js"></script>
+		<script type="text/javascript">
+		
 		function derouler(){
 			elm = document.getElementById("friends");
 			if(elm.className == "unwrap"){
@@ -40,57 +45,11 @@
 				elm.className = "unwrap";
 			}
 		}
-		function getXhr(){
-			var xhr = null; 
-			if(window.XMLHttpRequest) // Firefox et autres
-			   xhr = new XMLHttpRequest(); 
-			else if(window.ActiveXObject){ // Internet Explorer 
-			   try {
-		                xhr = new ActiveXObject("Msxml2.XMLHTTP");
-		            } catch (e) {
-		                xhr = new ActiveXObject("Microsoft.XMLHTTP");
-		            }
-			}
-			else { // XMLHttpRequest non support� par le navigateur 
-			   alert("Votre navigateur ne supporte pas les objets XMLHTTPRequest..."); 
-			   xhr = false; 
-			}
-			return xhr;
-		}
-			
-		function searchUser(){
-			var xhr = getXhr();
-			//alert(getHostProjectName()); // /~vblum/RIsk_online/www/acceuil.php
-			//alert(window.location.href); // http://tp-ssh2.ups.u-psud.fr/~vblum/RIsk_online/www/acceuil.php
-			//var pathname = window.location.pathname;
-			//pathname = pathname.substring(0,17);
-			var url = getHostProjectName() + "/www/include/ajout_ami.php?";
-			//var url2 = "/~vblum/RIsk_online/www/include/ajout_ami.php?";
-			//alert(url);
-			//alert(url2);
-			//TODO onsubmit();
-			//var urlH = window.location.href;
-			//on construit l'url avec les params en GET
-			if($("#search_user").val() != null && $("#search_user").val() != ""){
-				url = url + "pseudoSearch=" + $("#search_user").val();
-			}
-			xhr.onreadystatechange = function(){
-				// On ne fait quelque chose que si on a tout re�u et que le serveur est ok
-				if(xhr.readyState == 4 && xhr.status == 200){
-					if(xhr.responseText != ""){
-						//alert("ca marche");
-						notif(xhr.responseText,1);
-					}
-				}
-			}
-			xhr.open("GET",url,true);	//On lance la page en GET
-			xhr.send(null);
-		}
 		
-		function getHostProjectName(){
-			var pathname = window.location.pathname;
-			var projectName = "/RIsk_online/";
-			pathname = pathname.substr(0,pathname.indexOf(projectName)+projectName.length);
-			return pathname;
+		function searchUser(){
+			var param = {
+				pseudoSearch: $("#search_user").val(),
+			};
+			reqAjax(param, "/www/include/ajout_ami.php", notif);
 		}
 		</script>
