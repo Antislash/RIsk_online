501e0c435b9f794b025c8966892c0b3cc57c65d1
busquets <alexis.pointurier@gmail.com>
2016-05-13 12:56:39 +0200

    Liste des rooms

---
+++ www/list-room.php
@@ -0,0 +1,462 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="style/style.css" />
+		<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+		<meta name="viewport" content="width=device-width" />
+		<title>Room</title>
+	</head>
+	<body>
+		<?php include('include/navigation.php');?>
+		<?php include('include/notif.php');?>
+		<?php include('../php/connexion.php');$image = "images/avatar.png";?>
+		<div class="bloc" id="list">
+			<div class="list-room">
+				<table>
+					<tr>
+						<td class="room-date">
+							01/01/2016
+						</td>
+						<td class="room-name">
+							Partie de Alexis
+						</td>
+						<td class="room-nb">
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+					<tr>
+						<td>
+							01/01/2016
+						</td>
+						<td>
+							Partie de Alexis
+						</td>
+						<td>
+							4/6
+						</td>
+					</tr>
+				</table>
+			</div>
+		</div>
+		<?php include('include/contact.php');?>
+	</body>
+</html>
