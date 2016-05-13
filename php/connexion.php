f8c1f9d2d6cda8a73e629f64db660ebbe6bfd24a
Vivien Blum <vivien.blum@a-109978.ups.u-psud.fr>
2016-05-13 09:55:02 +0200

    changement connexion.php

--- php/connexion.php
+++ php/connexion.php
@@ -1,8 +1,12 @@
 <?php
 
 try {
-
-    $bdd = new PDO('mysql:host=localhost;dbname=risk;charset=utf8', 'root', '');
+	//Pour se connecter en local au PUIO
+    //$bdd = new PDO('mysql:host=tp-sgbd;dbname=vblum_a;charset=utf8', 'vblum_a', 'vblum_a');
+    //Pour se connecter en local ailleurs
+    $bdd = new PDO('mysql:host=localhost;risk=vblum_a;charset=utf8', 'root', '');
+    //Pour se connecter sur le serveur
+    //TODO ...
 
 } catch (Exception $e) {
 
