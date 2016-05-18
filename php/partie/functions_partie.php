0ce99b0b67b7a94f6ba13f2a7f0715cda958085e
Vivien Blum <vivien.blum@a-106556.ups.u-psud.fr>
2016-05-18 10:55:40 +0200

    deplacement

--- php/partie/functions_partie.php
+++ php/partie/functions_partie.php
@@ -1,14 +1,33 @@
 <?php
-	function frontiereCommune(id_partie, id_joueur, id_pays1, id_pays2){
-		$req = $bdd->query("SELECT * FROM partie_has_joueur_has_pays WHERE id_partie = ".id_partie." AND id_joueur = ".id_joueur."");
-		$allPays = $req->fetch();
-		if($allPays != false){
-			foreach($allPays as $pays){
-				
-			}
-		}
-		else{
+	$paysVisited = array();
+	foreach($allPays as $pays){
+		$paysVisited[$pays['id_pays']] = false;
+	}
+
+	function canMove($id_partie, $id_joueur, $id_pays1, $id_pays2){
+		$allPays = $bdd->query("SELECT * FROM partie_has_joueur_has_pays WHERE id_partie = ".$id_partie." AND id_joueur = ".$id_joueur."");
+		return canMoveRecursive($id_partie, $id_joueur, $id_pays1, $id_pays2, $paysVisited);
+	}
+	
+	function canMoveRecursive($id_partie, $id_joueur, $id_pays1, $id_pays2){
+		if(isAllPaysVisited($paysVisited) == true){	//si on a parcouru tous les pays
 			return false;
 		}
+		else if(true){	//TODO si on est sur le bon pays
+			
+		}
+		else{
+			//TODO marquer le pays comme visited
+			return canMoveRecursive($id_partie, $id_joueur, /*pays voisin de $id_pays1*/, $id_pays2);
+		}
+	}
+	
+	function isAllPaysVisited(array $paysVisited){
+		foreach($paysVisited as $pays){
+			if($pays == false){
+				return false;
+			}
+		}
+		return true;
 	}
 ?>
