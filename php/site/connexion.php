bac846deac52dba8a3718a4187cb9820afbf0bfc
Terafu <maxime-dufour@outlook.fr>
2016-05-22 22:41:51 +0200

    Modif connexion.php pour serveur

--- php/site/connexion.php
+++ php/site/connexion.php
@@ -7,7 +7,9 @@ try {
 	//Pour se connecter en local au PUIO
     //$bdd = new PDO('mysql:host=tp-sgbd;dbname=vblum_a;charset=utf8', 'vblum_a', 'vblum_a');
     //Pour se connecter en local ailleurs
-    $bdd = new PDO('mysql:host=localhost;dbname=risk;charset=utf8', 'root', '');
+    //$bdd = new PDO('mysql:host=localhost;dbname=risk;charset=utf8', 'root', 'root');
+    $bdd = new PDO('mysql:host=ivgz2rnl5rh7sphb.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;dbname=m9ivktqfxzjgpbe3;charset=utf8', 'dm0pel1q4vrf307y', 'ex0g988gs33tlltu');
+
 
 } catch (Exception $e) {
 
